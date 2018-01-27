a06cc8fa43a5c532d91c000a4768a51e9cf3c948
Perry Xiang <1142320771@qq.com>
2018-01-27 17:48:37 +0800

    ES6 - Class - Submit_2018-01-27

--- JavascriptMethod.js
+++ JavascriptMethod.js
@@ -1,10 +1,31 @@
-function Person(name, age) {
-    if (this instanceof Person) {
-        this.name = name;
-        this.age = age;
-    }
-    else
-        return new Person(name, age);
-}
-var person = Person("Perry", 26);
-console.log(person.name);
+// 'use strict';
+// var Person = {
+//     name:"Perry Xiang",
+//     country:"Sichuan Province",
+//     fast:"fast"
+// };
+// Object.preventExtensions(Person);
+// Person.name = "Qin";
+// console.log(Person.name);
+// // Person.age = 26;
+// // console.log(Person.age);
+// delete Person.fast;
+// console.log(Person.fast);
+// console.log(Object.isSealed(Person));
+// Object.seal(Person);
+// // delete Person.country;
+// // console.log(Person.country);
+// console.log(Object.isSealed(Person));
+
+// // Update and Add after Seal
+// Person.name = "China";
+// console.log(Person.name);
+
+// // Freeze
+// console.log(Object.isFrozen(Person));
+// Object.freeze(Person);
+// console.log(Object.isFrozen(Person));
+// // Update and Add after freeze
+// Person.name = "America";
+// console.log(Person.name);
+console.log(applicationCache.status);
